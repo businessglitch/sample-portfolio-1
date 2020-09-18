f9908aa1809329fecc8f361f10c0c9d4fc79fa28
Fahad Hayat <fahadhayat@outlook.com>
2020-09-18 14:54:54 -0400

    Wroking sample website for clients

---
+++ src/styles/components/navigation.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="container">
+        <div className="nav-columns">
+          <div className="nav-column">
+            <div className="nav-label">Menu</div>
+            <ul className="nav-links">
+              <li>
+                <NavLink to="/about" exact>
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/blogs" exact>
+                  Blogs
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/" exact>
+                  Home
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className="nav-column">
+            <div className="nav-label">Contact</div>
+            <div className="nav-infos">
+              <ul className="nav-info">
+                <li className="nav-info-label">Email</li>
+                <li>
+                  <NavLink to="/contact" exact>
+                    Get in touch with us
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
